5c32d4ded7b95d8a335d292986583df5913bbe27
sholikin1941720140 <sholikin1941720140@gmail.com>
2023-05-15 09:58:11 +0700

    firebase

---
+++ myapp/src/firebase.config.js
@@ -0,0 +1,12 @@
+import firebase from "firebase";
+
+export default {
+    apiKey: "AIzaSyBweRU7OJqLF5HvRbVY6b9VT5W69R82qHk",
+    authDomain: "fir-10-39494.firebaseapp.com",
+    projectId: "fir-10-39494",
+    storageBucket: "fir-10-39494.appspot.com",
+    messagingSenderId: "251849058699",
+    appId: "1:251849058699:web:b61b936765833c9f47e78e",
+    measurementId: "G-QY3SZBH3VN"
+};
+export const googleProvider = new firebase.auth.GoogleAuthProvider();
